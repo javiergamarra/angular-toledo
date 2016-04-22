db076e952b7bc0f02eff861cc4a4ee3c087f43ff
Javier Gamarra <nhpatt@gmail.com>
2016-04-23 00:53:22 +0200

    use an enum to model the status of the field

--- app/app.component.ts
+++ app/app.component.ts
@@ -14,6 +14,10 @@ export class AppComponent {
 
 class Option {
 
+    private status:OptionStatus;
+
     constructor(public name, public description, public votes = 0) {
     }
 }
+
+enum OptionStatus { Abandoned, Active }
