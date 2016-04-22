86f855096254866225fc1da1e44169a425055426
Javier Gamarra <nhpatt@gmail.com>
2016-04-23 00:50:35 +0200

    use ngIf to filter elements

--- app/app.component.ts
+++ app/app.component.ts
@@ -7,8 +7,8 @@ import {Component, OnInit} from 'angular2/core';
 export class AppComponent {
 
     private options:Option[] = [
-        new Option('toledo is awesome', 'because marzipan', 20),
-        new Option('valladolid is cool', 'because lechazo', 20)
+        new Option('toledo is awesome', 'because marzipan', 10),
+        new Option('valladolid is cool', 'because lechazo')
     ]
 }
 
