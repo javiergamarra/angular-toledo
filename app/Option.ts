754b6fe2536e5d394c1aea5ee91fc980ac52507b
Javier Gamarra <nhpatt@gmail.com>
2016-04-23 02:35:48 +0200

    render an image in each row

--- app/Option.ts
+++ app/Option.ts
@@ -1,6 +1,8 @@
 export class Option {
 
     private status:OptionStatus;
+    private image:string =
+        'https://news.ycombinator.com/grayarrow2x.gif1';
 
     constructor(public name, public description?, public votes = 0) {
     }
