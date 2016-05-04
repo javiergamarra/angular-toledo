bbc9c9645c148cecad908b0856767e385fd94358
Javier Gamarra <nhpatt@gmail.com>
2016-05-04 13:07:35 +0200

    insert in submit component

--- app/submit.component.ts
+++ app/submit.component.ts
@@ -1,16 +1,23 @@
 import {Component} from "angular2/core";
 import {Option} from "./Option";
+import {OptionsService} from "./OptionsService";
 
 @Component({
     templateUrl: 'submit.html',
-    selector: 'submit'
+    selector: 'submit',
+    providers: [OptionsService]
 })
 export class SubmitComponent {
 
     public option:Option = new Option('');
 
+    constructor(private optionsService:OptionsService) {
+    }
+
     onSubmit() {
-        console.log('submitted');
+        this.optionsService.addOption(this.option).subscribe(x => {
+            console.log('Submitted!')
+        })
     }
 
 }
