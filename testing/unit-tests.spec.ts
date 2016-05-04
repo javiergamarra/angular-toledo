ec37529a748ec1d7451f96ba199dfb93d99c151f
Javier Gamarra <nhpatt@gmail.com>
2016-05-04 19:29:21 +0200

    fix test :P

--- testing/unit-tests.spec.ts
+++ testing/unit-tests.spec.ts
@@ -45,7 +45,7 @@ describe('1st tests', () => {
         () => {
 
             let iterable = [3, 5, 7];
-            // iterable.foo = "hello";
+            iterable.foo = "hello";
 
             let for_props = 0;
             let for_of_props = 0;
